7fdde3281572c0a07f7fa95bd3ac984ddf619ca9
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-11-16 19:47:58 +0100

    Initial commit

---
+++ src/main/java/com/zikozee/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.zikozee.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtoolApplication.class, args);
+    }
+
+}
