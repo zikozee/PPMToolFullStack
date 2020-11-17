c60b0818467be305db87813b2e30b3686a32855f
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-11-17 14:31:22 +0100

    Validation showing custom formatted errors. closes #5

--- src/main/java/com/zikozee/ppmtool/project/ProjectController.java
+++ src/main/java/com/zikozee/ppmtool/project/ProjectController.java
@@ -2,15 +2,19 @@ package com.zikozee.ppmtool.project;
 
 import com.zikozee.ppmtool.project.dto.ProjectDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/project")
@@ -22,8 +26,11 @@ public class ProjectController {
     @PostMapping
     public ResponseEntity<?> createNewProject(@Valid @RequestBody ProjectDTO projectDTO, BindingResult result){
         if(result.hasErrors()){
-            return new ResponseEntity<>("Invalid Object", HttpStatus.BAD_REQUEST);
+            Map<String, String> errorMap = result.getFieldErrors().stream()
+                    .collect(Collectors.toMap(FieldError::getField, DefaultMessageSourceResolvable::getDefaultMessage));
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
         }
+
         ProjectDTO response = projectService.saveOrUpdateProject(projectDTO);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
