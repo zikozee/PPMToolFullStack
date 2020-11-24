d3f4e9d34ed15e4f946c362355bb19a36e11d977
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-11-24 12:32:24 +0100

    Add CrossOrigin to React Client. closes #21

--- src/main/java/com/zikozee/ppmtool/project/ProjectController.java
+++ src/main/java/com/zikozee/ppmtool/project/ProjectController.java
@@ -16,7 +16,6 @@ import java.util.List;
 @RestController
 @RequestMapping("api/project")
 @RequiredArgsConstructor
-@CrossOrigin({"http://localhost:3000"})
 public class ProjectController {
 
     private final ProjectService projectService;
